84e63ee29c419fb81c53053a6af35daf546e6c0e
astricklandd <111793985+astricklandd@users.noreply.github.com>
2022-09-12 16:23:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Strickland's Shop </title>
+    </head>
+    <body>
+        <h2>Strickland's Online Shop</h2>
+        <form action="authenticate.php" method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit">
+        </form>
 
+    </body>
+</html>
