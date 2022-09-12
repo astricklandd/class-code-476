8a5bf0dd196664c15e5aea2c34e7bd7babaa9c58
astricklandd <111793985+astricklandd@users.noreply.github.com>
2022-09-12 16:24:02 -0500

    Update secret.php

--- secret.php
+++ secret.php
@@ -1,1 +1,25 @@
+<?php
+session_start();
+// if username saved, let them see this page
+if (empty($_SESSION['username'])) {
+    // redirect
+    header("Location: index.php");
+} else {
+    $user = $_SESSION['username'];
+}
+// else, redirect to index.php
+?>
 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Hello, <?php echo($user) ?> <br>
+        Our secrets go here:-)
+      
+    </body>
+</html>
