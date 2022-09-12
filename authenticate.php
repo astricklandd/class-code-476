a7c0a7bf81d4766b314ef86a993edfd2982c8553
astricklandd <111793985+astricklandd@users.noreply.github.com>
2022-09-12 16:23:22 -0500

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+
+var_dump($_POST);
+ // if password == bar, save username for later
+
+if ($_POST['password'] === "bar") {
+    $_SESSION['username'] =  $_POST['username'];
+    header("Location:secret.php");
+}
+
 
